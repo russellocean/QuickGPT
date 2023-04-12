01b6f59a0ea7a3fcf0eced281023f4709dbac952
russellocean <russellwelch17@gmail.com>
2023-04-12 16:07:15 -0400

    Custom agent components

--- agent_utils.py
+++ agent_utils.py
@@ -1,11 +1,12 @@
 import os
 
-from langchain.agents import ZeroShotAgent, Tool, AgentExecutor
 from langchain import LLMChain
 from langchain.chat_models import ChatOpenAI
 from langchain.utilities.wolfram_alpha import WolframAlphaAPIWrapper
 from langchain.utilities import GoogleSerperAPIWrapper
-from langchain.agents import Tool
+from langchain.agents import Tool, AgentExecutor, LLMSingleActionAgent
+
+from agent_components import CustomPromptTemplate, CustomOutputParser
 
 import openai
 
@@ -31,27 +32,54 @@ def setup_agent():
             description="Useful for when you need to answer questions about math, science, geography."
         )
     ]
+    
+    # Set up the base template
+    template = """Answer the following questions as best you can, but speaking as a pirate might speak. You have access to the following tools:
 
-    prefix = """Answer the following questions as best you can. You have access to the following tools:"""
-    suffix = """If answering a coding question only provide code, add comments to communicate your thought process.
+    {tools}
+
+    Use the following format:
+
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question
+    
+    If answering a coding question only provide code, add comments to communicate your thought process.
     If given multiple answer choices only provide the letter or answer choice.
     If answering a free response question only provide the answer.
 
+    Begin!
+
     Question: {input}
     {agent_scratchpad}"""
-
-    prompt = ZeroShotAgent.create_prompt(
-        tools, 
-        prefix=prefix, 
-        suffix=suffix, 
-        input_variables=["input", "agent_scratchpad"]
+    
+    prompt = CustomPromptTemplate(
+        template=template,
+        tools=tools,
+        # This omits the `agent_scratchpad`, `tools`, and `tool_names` variables because those are generated dynamically
+        # This includes the `intermediate_steps` variable because that is needed
+        input_variables=["input", "intermediate_steps"]
     )
+    
+    output_parser = CustomOutputParser()
+    
+    llm = ChatOpenAI(temperature=0.5, model="gpt-4")
 
-    llm_chain = LLMChain(llm=ChatOpenAI(temperature=0.5, model="gpt-4"), prompt=prompt)
+    llm_chain = LLMChain(llm=llm, prompt=prompt)
+    
     tool_names = [tool.name for tool in tools]
-
-    agent = ZeroShotAgent(llm_chain=llm_chain, allowed_tools=tool_names)
-
+    agent = LLMSingleActionAgent(
+        llm_chain=llm_chain, 
+        output_parser=output_parser,
+        stop=["\nObservation:"], 
+        allowed_tools=tool_names
+    )
+    
     agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True)
 
     return agent_executor
